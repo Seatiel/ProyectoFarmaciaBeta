578b694fafb2098cd829e7a1333eb7f6ec6e341a
iblamer <fabio_dlc96@hotmail.com>
2017-07-11 18:02:16 -0800

    Add files via upload
    intento

---
+++ Data/Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoBeta.Models;
+
+namespace ProyectoBeta.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=clientesserverdb.database.windows.net;Initial Catalog=FarmaciaDb;Integrated Security=False;User ID=matap91;Password=theworlds.91;Connect Timeout=15;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<ProyectoBeta.Models.Laboratorios> Laboratorios { get; set; }
+
+        public DbSet<ProyectoBeta.Models.Categorias> Categorias { get; set; }
+
+        public DbSet<ProyectoBeta.Models.Medicinas> Medicinas { get; set; }
+
+        public DbSet<ProyectoBeta.Models.TipoVentas> TipoVentas { get; set; }
+
+        public DbSet<ProyectoBeta.Models.VentasDetalle> VentasDetalle { get; set; }
+
+        public DbSet<ProyectoBeta.Models.Ventas> Ventas { get; set; }
+    }
+}
---
+++ Data/ScriptFarmaciaDb.sql
@@ -0,0 +1,53 @@
+﻿create database FarmaciaDb
+Create table Usuarios (
+UsuarioId int identity(1,1) primary key,
+Nombres varchar(80),
+NombreUsuario varchar(80),
+Clave Varchar(15),
+TipoUsuario int
+);
+
+Create table Categorias(
+CategoriaId int identity(1,1) primary key,
+Descripcion varchar(80)
+);
+
+Create table Laboratorios(
+LaboratorioId int identity(1,1) primary key,
+Nombre varchar(80),
+FechaIngreso date
+);
+
+Create table Medicinas(
+MedicinaId int identity(1,1) primary key,
+Nombre varchar(80),
+Descripcion varchar(80),
+PrecioVenta money,
+PrecioCompra money,
+FechaVencimiento date,
+CantidadExistencia int,
+LaboratorioId int,
+Especificaciones varchar(100),
+CategoriaId int
+);
+
+
+Create table TipoVentas(
+TipoVentaId int identity(1,1) primary key,
+Descripcion varchar(80)
+);
+
+create table VentasDetalle(
+DetalleId int identity(1,1) primary key,
+VentaId int,
+Cantidad int
+);
+
+Create table Ventas(
+VentaId int identity(1,1) primary key,
+TipoVentaId int,
+Medicina varchar(80),
+Cantidad int,
+FechaVenta date,
+Total money
+);
